e9cc4338bf683f60e37691582206b2a125bee4a5
Alexander Dobin <dobin@cshl.edu>
2014-09-07 16:26:45 -0400

    Removed gitVersion.sh, was not useful

--- gitVersion.sh
+++
@@ -1,7 +0,0 @@
-git rev-parse --is-inside-work-tree >& gitCheck.tmp
-if [ "$(<gitCheck.tmp)" == "true" ] 
-then
-	#echo "#define STAR_VERSION \"`git tag; git rev-parse HEAD`\"" | tr '\n' ' ' > VERSION
-	echo "#define STAR_VERSION \"`git tag`\"" | tr '\n' ' ' > VERSION
-fi
-rm gitCheck.tmp
