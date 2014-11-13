a977b3cdc3f068b80df4d29100be845cae82ae48
Alexander Dobin <dobin@cshl.edu>
2014-11-13 18:46:30 -0500

    Experimenting with optimization, replacing long seed with overlapping short seeds does not work

--- source/ReadAlign_storeAligns.cpp
+++ source/ReadAlign_storeAligns.cpp
@@ -49,79 +49,60 @@ void ReadAlign::storeAligns (uint iDir, uint Shift, uint Nrep, uint L, uint indS
         exitWithError(errOut.str(),std::cerr, P->inOut->logMain, EXIT_CODE_RUNTIME, *P);
     };    
   #else
-    int iP3;
-    for (iP3=nP-1; iP3>=0; iP3--) {
-        if ( PC[iP3][0]<=rStart ) {
-            if ( (PC[iP3][PC_rStart]==rStart) && PC[iP3][PC_Length]<L ) iP3=-1;
-            if ( (PC[iP3][PC_rStart]==rStart) && PC[iP3][PC_Length]==L ) return; //same alignment as before, do not store!
-            break;
-        };
-    };
+//     int iP3;
+//     for (iP3=nP-1; iP3>=0; iP3--) {
+//         if ( PC[iP3][0]<=rStart ) {
+//             if ( (PC[iP3][PC_rStart]==rStart) && PC[iP3][PC_Length]<L ) continue;
+//             if ( (PC[iP3][PC_rStart]==rStart) && PC[iP3][PC_Length]==L ) iP3=-1; //same alignment as before, do not store!
+//             break;
+//         };
+//     };
     
-    int iP2=-1;
+    int iP2=-1,iP1;
     int nRemove=0;
-    for (int iP1=0; iP1<nP; iP1++) {
+    for (iP1=0; iP1<nP; iP1++) {
         if ( (PC[iP1][PC_rStart]==rStart) && PC[iP1][PC_Length]==L ) return; //exactly the same piece
-        if ( rStart >= PC[iP1][PC_rStart] ) {
+        if ( rStart >= PC[iP1][PC_rStart] ) {//is new seed within an old seed
             if ( rStart+L <= PC[iP1][PC_rStart]+PC[iP1][PC_Length] ) {//new piece is within the old piece
                 //decide whether to keep the old one
-                if ( false && (PC[iP1][PC_Nrep]>P->winAnchorMultimapNmax || Nrep<=P->winAnchorMultimapNmax)) {//old piece is not anchor, or new piece is anchor
+                if ( (PC[iP1][PC_Nrep]>P->winAnchorMultimapNmax || Nrep<=P->winAnchorMultimapNmax)) {//old piece is not anchor, or new piece is anchor
                     PC[iP1][PC_Dir]=-1;//do not keep the old piece
                     ++nRemove;
-                    if (iP2==-1) {
-                        iP2=iP1; //insertion point: before iP1;
-                    };                    
-                } else {
-                    //keep the old piece
-                    if (iP2==-1) {
-                        iP2=iP1+1; //insertion point: after iP1;
-                    };                    
-                };
-            } else {//new end extends past old end
-                //this is the insertion point for the new piece
-                if (iP2==-1) {
-                    iP2=iP1+1; //insertion point: after iP1
                 };
             };
         };
-        if ( rStart <= PC[iP1][PC_rStart] )  {// rStart <= PC[iP1][PC_rStart]
+        
+        if ( rStart <= PC[iP1][PC_rStart] )  {//is old seed within new seed
             if ( rStart+L >= PC[iP1][PC_rStart]+PC[iP1][PC_Length] ) {//old piece is within the new piece
                 //decide whether to keep the new piece
-                if ( false && (Nrep>P->winAnchorMultimapNmax || PC[iP1][PC_Nrep]<=P->winAnchorMultimapNmax) ) {//new piece is not anchor, or old piece is an anchor
+                if ( (Nrep>P->winAnchorMultimapNmax || PC[iP1][PC_Nrep]<=P->winAnchorMultimapNmax) ) {//new piece is not anchor, or old piece is an anchor
                     if (nRemove>0) {//debug
-                        cout << "BUG: nRemove="<<nRemove<<flush;
+                        cout << "BUG: nRemove="<<nRemove<<" iRead="<<iRead<<flush;
                         exit(-1);
                     };
                     return;//do not store the new piece
-                } else {
-                    //keep the new piece
                 };
-            } else {//old end extends past new end
-                //no more overlap possible, break the cycle
-                if (iP2==-1) {
-                    if (iP1!=0) {//debug
-                        cout << "BUG: iP1=0 iRead="<<iRead<<"   "<<readName<<flush;
-                        exit(-1);
-                    };  
-                    iP2=iP1;
-                };
-                break;
             };
         };
+        
+        if ( iP2==-1 && ( rStart < PC[iP1][PC_rStart] || (rStart == PC[iP1][PC_rStart] && L>PC[iP1][PC_Length]) ) ) {
+            iP2=iP1;
+        };
+        
 
     };
 
-    if (nP==0) iP2=0;
+    if (iP2==-1 && iP1==nP) iP2=nP;//    if (nP==0) iP2=0;
     if (iP2==-1) {//debug
         cout << "BUG: iP2=-1 iRead="<<iRead<<flush;
         exit(-1);
     };   
     
     int iP=iP2;
-    if (iP!=iP3+1) {
-        cout << "BUG: iP!=iP3+1 iRead="<<iRead<<"   "<<readName<<flush;
-        exit(-1);
-    };
+//     if (iP!=iP3+1) {
+//         cout << "BUG: iP!=iP3+1 iRead="<<iRead<<"   "<<readName<<flush;
+//         exit(-1);
+//     };
     
     if (nRemove==0) {//add piece
         if (nP == P->seedPerReadNmax) {
@@ -134,11 +115,17 @@ void ReadAlign::storeAligns (uint iDir, uint Shift, uint Nrep, uint L, uint indS
             for (int jj=0;jj<PC_SIZE;jj++) PC[ii+1][jj]=PC[ii][jj];
         };
         ++nP;
-    } else if (nRemove>1) {//replace in place
-        int iP3=iP2+1;
-        for (int ii=iP2+1; ii<nP; ii++) {//move old entries to free space for the new one
+    } else {//replace in place
+        int iP3=0;
+        for (int ii=0; ii<nP; ii++) {//move old entries to free space for the new one
+            if (ii==iP) {//empty slot for insertion
+                iP=iP3;
+                ++iP3;
+            };
             if ( PC[ii][PC_Dir] != -1) {//move this record to the new place
-                for (int jj=0;jj<PC_SIZE;jj++) PC[iP3][jj]=PC[ii][jj];//TODO use memcopy
+                if (ii!=iP3) {
+                    for (int jj=0;jj<PC_SIZE;jj++) PC[iP3][jj]=PC[ii][jj];//TODO use memcopy
+                };
                 ++iP3;
             };
         };        
