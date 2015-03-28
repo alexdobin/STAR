b9fb1a88e3dde08d5e3782680069009e7a79d33a
Brian Haas <brianjohnhaas@gmail.com>
2015-03-28 10:17:31 -0400

    renamed to STAR-Fusion

--- extras/star-fusion/test/runMe.sh
+++ extras/star-fusion/test/runMe.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-../star-fusion -S data/Chimeric.out.sam.gz -J data/Chimeric.out.junction.gz -G data/gencode.v19.rna_seq_pipeline.gtf.exons.gz
+../STAR-Fusion -S data/Chimeric.out.sam.gz -J data/Chimeric.out.junction.gz -G data/gencode.v19.rna_seq_pipeline.gtf.exons.gz
