540ed906ec2c9799ca73a91ebbcf4460d3631b71
Brian Haas <brianjohnhaas@gmail.com>
2015-04-12 14:41:38 -0400

    reorg

--- extras/star-fusion/test/runMe.sh
+++ extras/star-fusion/test/runMe.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-../STAR-Fusion -S data/Chimeric.out.sam.gz -J data/Chimeric.out.junction.gz -G data/gencode.v19.rna_seq_pipeline.gtf.exons.gz
+../STAR-Fusion -S data/Chimeric.out.sam.gz -J data/Chimeric.out.junction.gz
