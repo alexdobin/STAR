ca6b4e9b5ad6dc694e13522e160d5326e33fe29e
Alexander Dobin <dobin@cshl.edu>
2017-08-22 12:20:14 -0400

    Revert "Tried to fix memory leakage in ChimericAligns by replacing Transcript pointers. Too slow, will revert to the previous commit."
    This reverts commit dab33cfaa67c504b192019e2641d5275fb68556a.

--- source/ChimericAlign.cpp
+++ source/ChimericAlign.cpp
@@ -4,23 +4,27 @@ ChimericAlign::ChimericAlign(ChimericSegment &seg1in, ChimericSegment &seg2in, i
 {
     stitchingDone=false;
     
-    if (seg1.align.roStart <= seg2.align.roStart) {
-        al1=seg1.align;
-        al2=seg2.align;  
-    } else {
-        al1=seg2.align;
-        al2=seg1.align;
-    };
+    al1=&seg1.align;
+    al2=&seg2.align;
     
-    ex1 = al1.Str==1 ? 0 : al1.nExons-1;
-    ex2 = al2.Str==0 ? 0 : al2.nExons-1;
+    if (al1->roStart > al2->roStart) 
+        swap (al1,al2);
+            
+    ex1 = al1->Str==1 ? 0 : al1->nExons-1;
+    ex2 = al2->Str==0 ? 0 : al2->nExons-1;
+};
+
+ChimericAlign::~ChimericAlign()
+{
+    delete al1;
+    delete al2;
 };
 
 bool ChimericAlign::chimericCheck()
 {
     bool chimGood=true;
     
-    chimGood = chimGood && al1.exons[ex1][EX_iFrag] <= al2.exons[ex2][EX_iFrag];//otherwise - strange configuration, both segments contain two mates
+    chimGood = chimGood && al1->exons[ex1][EX_iFrag] <= al2->exons[ex2][EX_iFrag];//otherwise - strange configuration, both segments contain two mates
         //if ( trChim[0].exons[e0][EX_iFrag] > trChim[1].exons[e1][EX_iFrag] ) {//strange configuration, rare, similar to the next one
         //    chimN=0;//reject such chimeras
             //good test example:
@@ -28,7 +32,7 @@ bool ChimericAlign::chimericCheck()
             //TTGAGGTGAAGTTCAAAGATGTGGCTGGCTGTGAGGAGGCCGAGCTAGAGATCATGGAATTTGTGAATTTCTTGAA
         //} else 
 
-    chimGood = chimGood && al1.exons[ex1][EX_L] >= pCh.junctionOverhangMin &&  al2.exons[ex2][EX_L] >= pCh.junctionOverhangMin;//junction overhangs too short                     
+    chimGood = chimGood && al1->exons[ex1][EX_L] >= pCh.junctionOverhangMin &&  al2->exons[ex2][EX_L] >= pCh.junctionOverhangMin;//junction overhangs too short                     
 
     return chimGood;
 };
--- source/ChimericAlign.h
+++ source/ChimericAlign.h
@@ -12,13 +12,14 @@ class ChimericAlign
     public:
         ChimericSegment  seg1, seg2;     //two chimeric segments
 //         std::unique_ptr <Transcript> al1, al2; //two chimeric alignments - modified by chimeric switching
-        Transcript al1, al2;
+        Transcript *al1, *al2;
         uint ex1, ex2;
         
         uint chimJ1, chimJ2, chimRepeat1, chimRepeat2;
         int chimMotif, chimStr, chimScore;
         
         ChimericAlign(ChimericSegment &seg1in, ChimericSegment &seg2in, int chimScoreIn); //allocate
+        ~ChimericAlign();
         void chimericJunctionOutput(fstream &outStream, uint chimN);
         void chimericStitching(char *genSeq, char *readSeq);
         bool chimericCheck();
