acdf43044f5c1b42f5f875316e82e02f0804d8fb
Alexander Dobin <dobin@vega.cshl.edu>
2018-10-29 15:20:16 -0400

    Implementing gene from transcript alignment

--- source/SoloCB_readCB.cpp
+++ source/SoloCB_readCB.cpp
@@ -6,22 +6,38 @@ void SoloCB::readCB(const string &readNameExtra, const uint nTr, const vector<in
     if (pSolo.type==0)
         return;
     
-//     if (nTr > 1) //unique mappers only for now
-// maps        return;
-    
     int32 rG=readGene.at(P.pReads.strand);
+
+//     if (nTr > 1) //unique mappers only for now
+//        return;
+
+    if (nTr > 1)
+        rG=-3;
+
     
 //     if (rG < 0) //only non-ambiguous genes for now
 //         return;
     
-    uint32 cbB;
-    if (!convertNuclStrToInt32(readNameExtra.substr(0,pSolo.cbL),cbB))
-        return; //non-ACGT symbols
-    
-    int32 cbI=binarySearchExact(cbB,pSolo.cbWL.data(),pSolo.cbWL.size());
-    
-    if (cbI>=0) {
-        *strU_0 << cbI <<' '<<rG<< '\n';
+    uint32 cbB, umiB;
+    int32 cbI=-2;
+    if (convertNuclStrToInt32(readNameExtra.substr(0,pSolo.cbL),cbB) \
+        && convertNuclStrToInt32(readNameExtra.substr(pSolo.cbL,pSolo.umiL),umiB)) {
+        cbI=binarySearchExact(cbB,pSolo.cbWL.data(),pSolo.cbWL.size());
     } else {
+        return; //TODO stats for non-recorded
+    };
+
+    if (cbI!=-1) {
+        *strU_0 << cbI <<' '<< rG <<' '<< umiB <<'\n';
+    } else {
+        for (uint32 ii=0; ii<2*pSolo.cbL; ii+=2) {
+            for (uint32 jj=1; jj<4; jj++) {
+                cbI=binarySearchExact(cbB^(jj<<ii),pSolo.cbWL.data(),pSolo.cbWL.size());
+                if (cbI>=0) {
+                    *strU_1 << cbI <<' '<< readNameExtra.at(pSolo.cbL+pSolo.umiL+1+ii/2) <<' ';
+                };
+            };
+        };
+        *strU_1 << rG <<' '<< umiB <<'\n';
     };   
 };
--- source/Transcriptome_quantAlign.cpp
+++ source/Transcriptome_quantAlign.cpp
@@ -44,33 +44,28 @@ int alignToTranscript(Transcript &aG, uint trS1, uint8 trStr1, uint32 *exSE1, ui
             case -999: //last exon
                 if (trStr1==2) {//convert align coordinates if on the -strand
                     uint32 trlength=exLenCum1[exN1-1]+exSE1[2*exN1-1]-exSE1[2*exN1-2]+1; //transcript length
-                    for (uint32 iex=0; iex<aT.nExons; iex++)
-                    {
+                    for (uint32 iex=0; iex<aT.nExons; iex++) {
                         aT.exons[iex][EX_R]=aG.Lread-(aT.exons[iex][EX_R]+aT.exons[iex][EX_L]);
                         aT.exons[iex][EX_G]=trlength-(aT.exons[iex][EX_G]+aT.exons[iex][EX_L]);
                     };
-                    for (uint32 iex=0; iex<aT.nExons/2; iex++)
-                    {
+                    for (uint32 iex=0; iex<aT.nExons/2; iex++) {
                         swap(aT.exons[iex][EX_R],aT.exons[aT.nExons-1-iex][EX_R]);
                         swap(aT.exons[iex][EX_G],aT.exons[aT.nExons-1-iex][EX_G]);
                         swap(aT.exons[iex][EX_L],aT.exons[aT.nExons-1-iex][EX_L]);
                         swap(aT.exons[iex][EX_iFrag],aT.exons[aT.nExons-1-iex][EX_iFrag]);
                     };
-                    for (uint32 iex=0; iex<(aT.nExons-1)/2; iex++)
-                    {
+                    for (uint32 iex=0; iex<(aT.nExons-1)/2; iex++) {
                         swap(aT.canonSJ[iex],aT.canonSJ[aT.nExons-2-iex]);
                     };
-
                 };
-                for (uint32 iex=0; iex<aT.nExons; iex++)
-                {//no junctions in the transcritomic coordinates
+                for (uint32 iex=0; iex<aT.nExons; iex++) {//no junctions in the transcritomic coordinates
                     aT.sjAnnot[iex]=0;
                     aT.shiftSJ[iex][0]=0;
                     aT.shiftSJ[iex][1]=0;
                     aT.sjStr[iex]=0;
                 };
 
-                return 1; //reached the end of blocks, align is consistend with this transcript
+                return 1; //reached the end of blocks, align is consistent with this transcript
                 break;
             case -3: //mate connection
                 ex1=binarySearch1<uint32>(aG.exons[iab+1][EX_G]-trS1, exSE1, 2*exN1);
