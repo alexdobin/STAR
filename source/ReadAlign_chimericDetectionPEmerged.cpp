075a6db9bdec77815e4a4ef653a5fc969b87fd90
Brian Haas <brianjohnhaas@gmail.com>
2019-08-09 12:38:03 -0400

    use mergedPE align score for comparison w/ chimeric mergedPE score

--- source/ReadAlign_chimericDetectionPEmerged.cpp
+++ source/ReadAlign_chimericDetectionPEmerged.cpp
@@ -72,7 +72,7 @@ void ReadAlign::chimericDetectionPEmerged(ReadAlign &seRA) {
 
         // new chimeric detection routine
 
-        chimRecord=seRA.chimDet->chimericDetectionMult(seRA.nW, seRA.readLength, trBest->maxScore, true);
+        chimRecord=seRA.chimDet->chimericDetectionMult(seRA.nW, seRA.readLength, seRA.trBest->maxScore, true);
     };
 
     if ( chimRecord ) {
