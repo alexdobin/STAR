71d2850c77dddc1e7ce80cd65a85575c47b26620
Alexander Dobin <dobin@cshl.edu>
2019-10-21 12:07:49 -0400

    Release SpliceGraph_2019-10-20.

--- source/Transcript_convertGenomeCigar.cpp
+++ source/Transcript_convertGenomeCigar.cpp
@@ -18,12 +18,12 @@ bool Transcript::convertGenomeCigar(Genome &genOut, Transcript & A)
     A.cigar.reserve(cigar.size()+100); //add 100 for junctions
     
     uint64 gEnd=gStart;//gEnd is end+1
-    uint32 l1=0, l2=0;
-    uint32 gGapNew=0;
+    //uint32 l1=0, l2=0;
+    //uint32 gGapNew=0;
     for (uint32 iC=0; iC<cigar.size(); iC++) {
         auto cc=cigar[iC];        
-        if (cc[0]==BAM_CIGAR_M)
-            l1+=cc[1];
+        //if (cc[0]==BAM_CIGAR_M)
+        //    l1+=cc[1];
         if (cc[0]==BAM_CIGAR_M || cc[0]==BAM_CIGAR_D || cc[0]==BAM_CIGAR_N) {//M,D,N: blocks that have to be split if they overlap blocks boundaries
             uint64 gStart1=gEnd;
             gEnd += cc[1];
@@ -31,23 +31,15 @@ bool Transcript::convertGenomeCigar(Genome &genOut, Transcript & A)
             while (( bE=coBl[icB][0]+coBl[icB][1] ) <= gEnd) {//MDN-block ends at the c-block end, or goes past
                 uint32 len1 = (uint32)( bE-gStart1 );//first part of the a-block
                 
-                if (cc[0]==BAM_CIGAR_M)
-                    l2+=len1;
-                
-                if (cc[0]==A.cigar.back()[0]) {//this can only happen for N
-                    A.cigar.back()[1] += len1;//add to previous gap
-                } else {
-                    A.cigar.push_back({cc[0], len1});
-                };
+                //if (cc[0]==BAM_CIGAR_M)
+                //    l2+=len1;
+
+                A.cigar.push_back({cc[0], len1});
                 
                 len1 = (uint32)( coBl[icB+1][2]-coBl[icB][2]-coBl[icB][1] );//insert junction gap
-                gGapNew += len1;
+                //gGapNew += len1;
                 
-                if (A.cigar.back()[0]==BAM_CIGAR_N) {
-                    A.cigar.back()[1] += len1;//add to previous gap
-                } else {
-                    A.cigar.push_back({BAM_CIGAR_N, len1});
-                };
+                A.cigar.push_back({BAM_CIGAR_N, len1});
                 
                 gStart1=bE;
                 ++icB;
@@ -56,24 +48,24 @@ bool Transcript::convertGenomeCigar(Genome &genOut, Transcript & A)
             if (gEnd>gStart1)
                 A.cigar.push_back({ cc[0], (uint32) (gEnd-gStart1) });
               
-            if(cc[0]==BAM_CIGAR_M)
-                l2 += gEnd-gStart1;
-            if (l1!=l2) {
-                cout << l1 <<" "<< l2 <<endl;                
-            };
+            //if(cc[0]==BAM_CIGAR_M)
+            //    l2 += gEnd-gStart1;
+            //if (l1!=l2) {
+            //    cout << l1 <<" "<< l2 <<endl;                
+            //};
             
             if (gEnd<coBl[icB][0]) //step back
                 --icB;
                 
         } else {//S,I
-            l1+=cc[1];
-            l2+=cc[1];
+            //l1+=cc[1];
+            //l2+=cc[1];
             A.cigar.push_back(cc);
         };        
     };
-    if (l1!=l2) {
-        cout << l1 <<" "<< l2 <<endl;                
-    };
+    //if (l1!=l2) {
+    //    cout << l1 <<" "<< l2 <<endl;                
+    //};
     
     {//merge consecutive identical operations
         auto c0=A.cigar.begin();
@@ -94,11 +86,11 @@ bool Transcript::convertGenomeCigar(Genome &genOut, Transcript & A)
      
     {//remove last junction, or the junction before last S - it could appear if the last cigar M lands at the end of the c-block
         if (A.cigar.back()[0] == BAM_CIGAR_N) {
-            gGapNew-=A.cigar.back()[1];
+            //gGapNew-=A.cigar.back()[1];
             A.cigar.pop_back(); 
         };
         if (A.cigar.back()[0] == BAM_CIGAR_S && (*(A.cigar.end()-2))[0] == BAM_CIGAR_N) {
-            gGapNew-=(*(A.cigar.end()-2))[1];
+            //gGapNew-=(*(A.cigar.end()-2))[1];
             A.cigar.erase(A.cigar.end()-2);
         };
     };
@@ -150,6 +142,7 @@ bool Transcript::convertGenomeCigar(Genome &genOut, Transcript & A)
         };
     };
     
+    //TODO: also need to recalculate nMM
     A.gLength=0;
     for (const auto &cc : A.cigar)
         if (cc[0]==BAM_CIGAR_M || cc[0]==BAM_CIGAR_D || cc[0]==BAM_CIGAR_N)
