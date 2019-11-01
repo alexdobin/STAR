5f4dff2cc8370b15fbdfb595dfe16d79aee37993
Alexey <alexey.mipt@gmail.com>
2019-11-01 13:35:26 +0100

    add const
    - const char* R
    - const uiWA* WA

--- source/stitchWindowAligns.cpp
+++ source/stitchWindowAligns.cpp
@@ -400,7 +400,7 @@ void finalizeTranscript(ReadAlign *RA, Transcript **wTr, uint *nWinTr,
 }
 
 void stitchWindowAligns(uint iA, uint nA, int Score, bool WAincl[], uint tR2, uint tG2, const Transcript& trA,
-                        uint Lread, uiWA* WA, char* R, const Genome &mapGen,
+                        uint Lread, const uiWA* WA, const char* R, const Genome &mapGen,
                         const Parameters& P, Transcript** wTr, uint* nWinTr, ReadAlign *RA) {
     //recursively stitch aligns for one gene
     //*nWinTr - number of transcripts for the current window
--- source/stitchWindowAligns.h
+++ source/stitchWindowAligns.h
@@ -6,8 +6,8 @@
 #include "stitchAlignToTranscript.h"
 
 void stitchWindowAligns(uint iA, uint nA, int Score, bool WAincl[], uint tR2,
-                        uint tG2, const Transcript &trA, uint Lread, uiWA *WA,
-                        char *R, const Genome &mapGen, const Parameters &P,
+                        uint tG2, const Transcript &trA, uint Lread, const uiWA *WA,
+                        const char *R, const Genome &mapGen, const Parameters &P,
                         Transcript **wTr, uint *nWinTr, ReadAlign *RA);
 // recursively stitch aligns for one gene
 //*nWinTr - number of transcripts for the current window
