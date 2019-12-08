28121ffcd2ea5235fe15b5141b6a0821d0cd5a62
Sebastian Uhrig <s.uhrig@dkfz.de>
2019-12-08 17:03:53 +0100

    minor speed improvement: unpack PackedArray using bit mask instead of bit shifting

--- source/PackedArray.h
+++ source/PackedArray.h
@@ -27,7 +27,8 @@ inline uint PackedArray::operator [] (uint ii) {
    uint S=b%8;
 
    uint a1 = *((uint*) (charArray+B));
-   a1 = ((a1>>S)<<wordCompLength)>>wordCompLength;
+   a1 >>= S;
+   a1 &= bitRecMask;
    return a1;
 };
 
