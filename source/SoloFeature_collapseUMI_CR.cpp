af7b2ec9e1a5b4d356f82620ec29091ba9a571ae
Alexander Dobin <dobin@cshl.edu>
2020-12-25 19:15:43 -0500

    Special UB tag for MultiGene UMI in 1MM_CR option.

--- source/SoloFeature_collapseUMI_CR.cpp
+++ source/SoloFeature_collapseUMI_CR.cpp
@@ -48,11 +48,10 @@ void SoloFeature::collapseUMI_CR(uint32 iCB, uint32 *umiArray)
     };
     gReadS[nGenes]=rguStride*rN;//so that gReadS[nGenes]-gReadS[nGenes-1] is the number of reads for nGenes, see below in qsort
 
-    unordered_map<uint32, uint32> umiMaxGeneCount;//for each umi, max counts of reads per gene
+    //unordered_map<uint32, uint32> umiMaxGeneCount;//for each umi, max counts of reads per gene
     
     unordered_map <uint32, unordered_map<uint32,uint32>> umiGeneHash, umiGeneHash0;
                    //UMI                 //Gene //Count
-    map <uint32,uint32> geneCounts; //unordered_map to make it faster?
     
     unordered_map <uint32,uint32> umiCorrected;
 
@@ -92,7 +91,7 @@ void SoloFeature::collapseUMI_CR(uint32 iCB, uint32 *umiArray)
        
         if (pSolo.umiFiltering.MultiGeneUMI) {
             for (uint64 iu=0; iu<nU0*umiArrayStride; iu+=umiArrayStride) {
-                umiGeneHash0[umiArray[iu+0]][gID[iG]]+=umiArray[iu+1];//this sums read counts over UMIs that were collapsed
+                umiGeneHash0[umiArray[iu+0]][iG]+=umiArray[iu+1];//this sums read counts over UMIs that were collapsed
             };
         };       
        
@@ -115,9 +114,22 @@ void SoloFeature::collapseUMI_CR(uint32 iCB, uint32 *umiArray)
 
         if (pSolo.umiFiltering.MultiGeneUMI) {
             for (uint64 iu=0; iu<nU0*umiArrayStride; iu+=umiArrayStride) {
-                umiGeneHash[umiArray[iu+0]][gID[iG]]+=umiArray[iu+1];//this sums read counts over UMIs that were collapsed
+                umiGeneHash[umiArray[iu+0]][iG]+=umiArray[iu+1];//this sums read counts over UMIs that were collapsed
             };
-        } else {
+
+            if (readInfo.size()>0) {//record cb/umi for each read
+                for (uint32 iR=0; iR<gReadS[iG+1]-gReadS[iG]; iR+=rguStride) {//cycle over reads
+                    uint64 iread1 = rGU1[iR+rguR];
+                    readInfo[iread1].cb = indCB[iCB] ;
+                    uint32 umi=rGU1[iR+rguU];
+                    
+                    if (umiCorrected.count(umi)>0)
+                        umi=umiCorrected[umi]; //correct UMI
+                    readInfo[iread1].umi=umi;
+                };      
+            };            
+
+        } else {//MultiGeneUMI
             qsort(umiArray, nU0, umiArrayStride*sizeof(uint32), funCompareNumbers<uint32>);
             nU1=1;
             for (uint64 iu=umiArrayStride; iu<nU0*umiArrayStride; iu+=umiArrayStride) {
@@ -131,23 +143,18 @@ void SoloFeature::collapseUMI_CR(uint32 iCB, uint32 *umiArray)
             countCellGeneUMI[countCellGeneUMIindex[iCB+1] + pSolo.umiDedup.countInd.CR] = nU1;
             countCellGeneUMIindex[iCB+1] = countCellGeneUMIindex[iCB+1] + countMatStride;//iCB+1 accumulates the index
         };
-        
-        if (readInfo.size()>0) {//record cb/umi for each read
-            for (uint32 iR=0; iR<gReadS[iG+1]-gReadS[iG]; iR+=rguStride) {//cycle over reads
-                uint64 iread1 = rGU1[iR+rguR];
-                readInfo[iread1].cb = indCB[iCB] ;
-                uint32 umi=rGU1[iR+rguU];
-                
-                if (umiCorrected.count(umi)>0)
-                    umi=umiCorrected[umi]; //correct UMI
-                readInfo[iread1].umi=umi;
-            };      
-        };
+
     };
 
     if (pSolo.umiFiltering.MultiGeneUMI) {
         
-        for (auto &iu: umiGeneHash) {
+        vector<uint32> geneCounts(nGenes,0);
+        vector<unordered_set<uint32>> geneUmiHash;
+        
+        if (readInfo.size()>0)
+            geneUmiHash.resize(nGenes);
+        
+        for (auto &iu: umiGeneHash) {//loop over UMIs for all genes
                        
             uint32 maxu=0, maxg=-1;
             for (auto &ig : iu.second) {
@@ -158,8 +165,9 @@ void SoloFeature::collapseUMI_CR(uint32 iCB, uint32 *umiArray)
                     maxg=-1;
                 };
             };
+
             if ( maxg+1==0 )
-                continue; //this umi is not counted for any gene
+                continue; //this umi is not counted for any gene, because two genes have the same read count for this UMI
             
             for (auto &ig : umiGeneHash0[iu.first]) {//check that this umi/gene had also top count for uncorrected umis
                 if (ig.second>umiGeneHash0[iu.first][maxg]) {
@@ -167,18 +175,42 @@ void SoloFeature::collapseUMI_CR(uint32 iCB, uint32 *umiArray)
                     break;
                 };
             };
-            if ( maxg+1!=0 )
+
+            if ( maxg+1!=0 ) {//this UMI is counted
                 geneCounts[maxg]++;
-            
+                if (readInfo.size()>0)
+                    geneUmiHash[maxg].insert(iu.first);
+            };
         };
 
-        for (auto &ig: geneCounts) {
+        for (uint32 ig=0; ig<nGenes; ig++) {
+            if (geneCounts[ig] == 0)
+                continue; //no counts for this gene
             nGenePerCB[iCB]++;
-            nUMIperCB[iCB] += ig.second;
-            countCellGeneUMI[countCellGeneUMIindex[iCB+1] + 0] = ig.first;
-            countCellGeneUMI[countCellGeneUMIindex[iCB+1] + pSolo.umiDedup.countInd.CR] = ig.second;
+            nUMIperCB[iCB] += geneCounts[ig];
+            countCellGeneUMI[countCellGeneUMIindex[iCB+1] + 0] = gID[ig];
+            countCellGeneUMI[countCellGeneUMIindex[iCB+1] + pSolo.umiDedup.countInd.CR] = geneCounts[ig];
             countCellGeneUMIindex[iCB+1] = countCellGeneUMIindex[iCB+1] + countMatStride;//iCB+1 accumulates the index
         };
+        
+        if (readInfo.size()>0) {//record cb/umi for each read
+            for (uint32 iG=0; iG<nGenes; iG++) {//cycle over genes
+                uint32 *rGU1=rGU+gReadS[iG];            
+                for (uint32 iR=0; iR<gReadS[iG+1]-gReadS[iG]; iR+=rguStride) {//cycle over reads
+                    uint64 iread1 = rGU1[iR+rguR];
+                    readInfo[iread1].cb = indCB[iCB] ;
+                    uint32 umi=rGU1[iR+rguU];
+                    
+                    if (umiCorrected.count(umi)>0)
+                        umi=umiCorrected[umi]; //correct UMI
 
+                    if (geneUmiHash[iG].count(umi)>0) {
+                        readInfo[iread1].umi=umi;
+                    } else {
+                        readInfo[iread1].umi=0;
+                    };
+                };
+            };
+        };
     };
 };
